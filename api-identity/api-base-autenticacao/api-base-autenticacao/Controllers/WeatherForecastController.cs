01f21afdac3ad66d136dee33abf5c6e7a864398b
Johnatas <johnatas@t2s.com.br>
2023-07-16 13:47:04 -0300

    Added project with base controller using Entity Framework

---
+++ api-identity/api-base-autenticacao/api-base-autenticacao/Controllers/WeatherForecastController.cs
@@ -0,0 +1,108 @@
+
+using infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using model.Entities;
+using System;
+
+namespace api_base_autenticacao.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ApiContext _apiContext;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,
+            ApiContext apiContext)
+        {
+            _logger = logger;
+            _apiContext = apiContext;
+        }
+
+
+        [HttpGet("summaries")]
+        public IActionResult GetAllSumaries()
+        {
+           return Ok(Summaries);
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAll()
+        {
+            var weatherForecas = await _apiContext.weatherForecasts.ToListAsync();
+
+            return Ok(weatherForecas);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var weatherForecas = await _apiContext.weatherForecasts.FindAsync(id);
+
+            if (weatherForecas is null)
+                return NotFound();
+
+            return Ok(weatherForecas);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostWeatherForecast(WeatherForecast weatherForecast)
+        {
+            try
+            {
+                //Need create a DTO Mapping
+
+                weatherForecast.Id = 0;
+                _apiContext.weatherForecasts.Add(weatherForecast);
+                await _apiContext.SaveChangesAsync();
+
+
+                return CreatedAtAction(nameof(GetById), new { id = weatherForecast.Id }, weatherForecast);
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex, ex.Message);
+
+                return BadRequest("Erro ao salvar weatherForecast");
+
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> PostWeatherForecast(int id)
+        {
+            try
+            {
+                var weatherForecas = await _apiContext.weatherForecasts.FindAsync(id);
+
+                if (weatherForecas is null)
+                    return NotFound();
+
+
+                _apiContext.weatherForecasts.Remove(weatherForecas);
+                _apiContext.SaveChanges();
+
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex, ex.Message);
+
+                return BadRequest("Erro ao ao deletar weatherForecast");
+
+            }
+
+        }
+    }
+}
---
+++ api-identity/api-base-autenticacao/infrastructure/ApiContext.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using model.Entities;
+using System.Reflection.Metadata;
+
+namespace infrastructure
+{
+    public class ApiContext : DbContext
+    {
+        string _source = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=api-authentication;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        public DbSet<WeatherForecast> weatherForecasts { get; set; }
+       
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_source);
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WeatherForecast>()
+                .HasKey(w => w.Id);
+                
+        }
+
+    }
+}
