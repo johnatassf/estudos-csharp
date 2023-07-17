02f3c9a43db88f42be73dec740204dc574719a17
Johnatas <johnatas@t2s.com.br>
2023-07-16 21:57:15 -0300

    Add create token and create user

---
+++ api-identity/api-base-autenticacao/api-base-autenticacao/Controllers/UserManagerController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using model.Entities;
+using Model.Dto;
+using Model.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace api_base_autenticacao.Controllers
+{
+    [Controller]
+    [Route("authentication")]
+    public class UserManagerController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManage;
+
+        public UserManagerController(SignInManager<User> signInManage,
+           UserManager<User> userManager
+           )
+        {
+            _userManager = userManager;
+            _signInManage = signInManage;
+        }
+
+
+        [HttpPost("user")]
+        public async Task<IActionResult> CreateUser(UserManagerDto userManagerDto)
+        {
+            var user = await _userManager.FindByEmailAsync(userManagerDto.UserName);
+
+            if (user == null)
+            {
+                user = new User
+                {
+                    FirstName = userManagerDto.FirstName,
+                    LastName = userManagerDto.LastName,
+                    Email = userManagerDto.Email,
+                    UserName = userManagerDto.UserName,
+                };
+
+                var result = await _userManager.CreateAsync(user, "P@ssW0rd!");
+
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create a user");
+                }
+                else
+                {
+                    return Ok("User create with success");
+                }
+            }
+
+            return BadRequest("Create failed");
+
+
+        }
+
+        [HttpPost("token")]
+        public async Task<IActionResult> CreateToken(LoginUserDto loginUserDto)
+        {
+         if(!ModelState.IsValid)
+            return BadRequest("Verifique os dados informados");
+
+
+            var user = await _userManager.FindByEmailAsync(loginUserDto.Username);
+
+            if(user is not null)
+            {
+                var result = await _signInManage.CheckPasswordSignInAsync(user, loginUserDto.Password, false);
+
+                if (result.Succeeded)
+                {
+                    var claims = new Claim[]
+                       {
+                            new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                            new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName),
+                       };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("testsetsetastase"));
+                    var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                    var token = new JwtSecurityToken(
+                        "http://localhost:8888",
+                        "http://localhost:8888",
+                        claims,
+                        signingCredentials: creds,
+                        expires: DateTime.UtcNow.AddMinutes(20));
+
+
+                    return Created("", new
+                    {
+                        token = new JwtSecurityTokenHandler().WriteToken(token),
+                        expiration = token.ValidTo
+                    });
+                }
+            }
+
+            return BadRequest("Erro ao efetuar login");
+
+
+        }
+    }
+}
--- api-identity/api-base-autenticacao/api-base-autenticacao/Program.cs
+++ api-identity/api-base-autenticacao/api-base-autenticacao/Program.cs
@@ -1,6 +1,8 @@
 using infrastructure;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using Model.Entities;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,17 @@ builder.Services.AddIdentity<User, IdentityRole<long>>(cfg =>
     cfg.User.RequireUniqueEmail = true;
 
 }).AddEntityFrameworkStores<ApiContext>();
+
+builder.Services.AddAuthentication()
+    .AddJwtBearer(cfg =>
+    {
+        cfg.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidIssuer = "http://localhost:5164",
+            ValidAudience = "http://localhost:5164",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("1234"))
+        };
+    }); 
 
 builder.Services.AddDbContext<ApiContext>();
 
